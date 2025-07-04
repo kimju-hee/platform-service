5fe905c0c335e944228ddee38b9b59e9e980e113
YWS00 <wsyoon2000@gmail.com>
2025-07-04 15:30:45 +0900

    Update BookRegistered.java

--- src/main/java/miniprojectjo/domain/BookRegistered.java
+++ src/main/java/miniprojectjo/domain/BookRegistered.java
@@ -1,28 +1,40 @@
 package miniprojectjo.domain;
 
 import java.time.LocalDate;
-import java.util.*;
 import lombok.*;
-import miniprojectjo.domain.*;
 import miniprojectjo.infra.AbstractEvent;
 
-//<<< DDD / Domain Event
+// <<< DDD / Domain Event
 @Data
 @ToString
 public class BookRegistered extends AbstractEvent {
 
-    private Long id;
+    private Long id;           
     private String bookName;
     private String category;
     private Boolean isBestSeller;
     private String bookContent;
+    private String authorName;
+    private String summary;
+    private String coverImageUrl;
+    private Integer subscriptionFee;
 
+    //Book → Event 변환 생성자
     public BookRegistered(Book aggregate) {
         super(aggregate);
+        this.id = aggregate.getId();
+        this.bookName = aggregate.getBookName();
+        this.category = aggregate.getCategory();
+        this.isBestSeller = aggregate.getIsBestSeller();
+        this.bookContent = aggregate.getContent();
+        this.authorName = aggregate.getAuthorName();
+        this.summary = aggregate.getSummary();
+        this.coverImageUrl = aggregate.getCoverImageUrl();
+        this.subscriptionFee = aggregate.getSubscriptionFee();
     }
 
     public BookRegistered() {
         super();
     }
 }
-//>>> DDD / Domain Event
+// >>> DDD / Domain Event
